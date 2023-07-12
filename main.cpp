da21bca27a16da39998ba7ed98fddbd113150c38
ptablas <ptabla200@caledonian.ac.uk>
2023-07-12 17:00:04 +0100

    Static wall

--- main.cpp
+++ main.cpp
@@ -508,6 +508,8 @@ void close()
 	SDL_Quit();
 }
 
+int j = 0;
+
 int main(int argc, char* args[])
 {
 	//Start up SDL and create window
@@ -536,6 +538,8 @@ int main(int argc, char* args[])
 			//While application is running
 			while (!quit)
 			{
+				j++;
+
 				//Handle events on queue
 				while (SDL_PollEvent(&e) != 0)
 				{
@@ -553,45 +557,54 @@ int main(int argc, char* args[])
 				if (!gWindow.isMinimized())
 				{
 					//Clear screen
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
 					SDL_RenderClear(gRenderer);
 
 					/*/Render text textures			NO TEXTURES
 					gSceneTexture.render((gWindow.getWidth() - gSceneTexture.getWidth()) / 2, (gWindow.getHeight() - gSceneTexture.getHeight()) / 2);
 					*/
 
-					int padX = gWindow.getWidth() / 20;
-					int padY = gWindow.getHeight() * 2 / 5;
-					int padWidth = gWindow.getWidth() / 20;
-					int padHeight = gWindow.getHeight() / 5;
+					int wHeight = gWindow.getHeight();
+					int wWidth = gWindow.getWidth();
+
+					int padX = wWidth / 20;
+					int padY = wHeight * 2 / 5;
+					int padWidth = wWidth / 20;
+					int padHeight = wHeight / 5;
+
+					int wallX = padX / 3;
+					int wallY = wHeight / 20 / 3;
+					int wallW = wWidth * 19 / 20 + (padX / 3);
+					int wallH = wHeight * 19 / 20 + (wHeight / 20 / 3);
 
 					//Render LeftPad
 					SDL_Rect LeftPad = { padX, padY, padWidth, padHeight};
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 					SDL_RenderFillRect(gRenderer, &LeftPad);
 
 					//Render RightPad
 
 					SDL_Rect RightPad = { padX * 18,  padY, padWidth, padHeight };
-					SDL_SetRenderDrawColor(gRenderer, 100, 0x00, 0x00, 0xFF);
+					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 					SDL_RenderFillRect(gRenderer, &RightPad);
 
 					//Draw blue horizontal line
-					SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
-					SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+					//SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+					//SDL_RenderDrawLine(gRenderer, wWidth / 2, 0, wWidth / 2, wHeight);
 
 					//Render green outlined quad
-					SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+
+					SDL_Rect outlineRect = { wallX, wallY, wallW, wallH };
 					SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
 					SDL_RenderDrawRect(gRenderer, &outlineRect);
 
 
 
 					//Draw vertical line of yellow dots
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
-					for (int i = 0; i < SCREEN_HEIGHT; i += 4)
+					SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+					for (int i = 0; i < wHeight; i += 4)
 					{
-						SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+						SDL_RenderDrawPoint(gRenderer, wWidth / 2, i);
 					}
 
 					//Update screen
